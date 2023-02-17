ba046d2e73c6bd58bbe0411b4dd5bd2ec880e583
Mahalakshmi .S <46067421+s-mahalakshmi@users.noreply.github.com>
2023-02-17 14:48:48 +0530

    Add files via upload

---
+++ UserInterface.java
@@ -0,0 +1,22 @@
+Project Name: Simple Test Automation
+
+Test case:1
+
+S.No	Action/Description	           Input	             Expected Results 
+1	Click on “create new issue”      	NA	            Create new issuePage should be displayed.
+2	Click on ‘title’ text field       Issue 1	        It should accept/display  alphanumeric characters .
+3	Click on description text area 	Enterdescription    It should accept/display  alphanumeric characters .
+4	Click save button	                NA	            It should save the data.
+5	Click on home page	                NA	            It should display the title and description with an ‘edit' and ‘delete’ button.’
+
+
+Back-end
+
+
+Test case2:
+
+S.No	Action/Description	Input	                       Expected Results 
+1	Click on edit button	  NA	            It should enable edit mode on selected issue.(updated)
+2	Click on save	          NA	             It should save the updated  data.
+3	Click on delete button 	  NA                 It should delete the selected issue from storage and should reflect on the home page.
+
